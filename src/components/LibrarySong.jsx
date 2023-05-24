b83dcc393ec9fcc683d802f24593ff7f3cac176f
John Dungao <johnmiguel.dungao@gmail.com>
2023-05-24 04:06:55 -0400

    change music data

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const LibrarySong = ({song}) => {
+const LibrarySong = ({song, setCurrentSong, songs, id}) => {
    const {cover, name, artist} = song;
+
+   const songSelectHandler = () => {
+      const selectedSong = songs.filter(state => state.id === id);
+      setCurrentSong(selectedSong[0]);
+   }
+
    return (
-      <div className="library-song">
+      <div onClick={songSelectHandler} className="library-song">
          <img src={cover} alt={name}/>
          <div className="song-description">
             <h3>{name}</h3>
--- src/data/music-data.js
+++ src/data/music-data.js
@@ -1,112 +1,102 @@
-import {v4 as uuidv4 } from "uuid";
+ import {v4 as uuidv4 } from "uuid";
+const barcode_2 = 'https://i.scdn.co/image/ab67616d0000b27310b4062c31a9a7e5972abc96'
 
 function chillHop() {
    return [
       {
-         name: "Beaver Creek",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-         artist: "Aso, Middle School, Aviino",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-         color: ["#205950", "#2ab3bf"],
+         name: 'LATINA',
+         cover: 'https://i.scdn.co/image/ab67616d0000b273a1d66189547e0ab221ef8618',
+         artist: 'Al James',
+         color: ['#DFB464','#313232'],
          id: uuidv4(),
          active: true,
+         audio: 'https://audio.jukehost.co.uk/QgwostrQElx3xnmzFWebP6oB5ezR6D9u'
       },
       {
-         name: "Daylight",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-         artist: "Aiguille",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-         color: ["#EF8EA9", "#ab417f"],
+        name:'NGAYON GABI',
+         cover:'https://i1.sndcdn.com/artworks-000264783188-ljx5j5-t500x500.jpg',
+         artist:'Al James',
+         color:['#1C171A','#1C1519'],
          id: uuidv4(),
          active: false,
+         audio:'https://audio.jukehost.co.uk/6ure2RyoRnx0HwyRw94jOJOoU326este'
+      },{
+        name:'ATBP',
+         cover:'https://i.scdn.co/image/ab67616d0000b273e8f84141bd8d52c43b44440b',
+         artist:'Lo Ki',
+         color:['#551137','#120100'],
+         id: uuidv4(),
+         active: false,
+         audio:'https://audio.jukehost.co.uk/4T3hpmScCkzHArhXt7foqfH5NO6sF59G'
+      },{
+        name:'Elevate',
+         cover:'https://images.genius.com/96bd76d5ff5867affa5987a1c80694df.1000x1000x1.jpg',
+         artist:'Jeff Grecia',
+         color:['#E4E1DC', '#716058'],
+         id: uuidv4(),
+         active: false,
+         audio:'https://audio.jukehost.co.uk/CnH31k8L8QpBZRfg3gVF2LXREtUdVPSb'
+      },{
+        name:'Tao Lang',
+         cover:'https://i.scdn.co/image/ab67616d0000b273e98fb2c951203ee207e9a7e4',
+         artist:'Loonie',
+         color:['#FFFFFF','#D7D7D7'],
+         id: uuidv4(),
+         active: false,
+         audio:'https://audio.jukehost.co.uk/9uyWLCQndhip59cTf6hRyzlWUA8S5JTm'
       },
       {
-         name: "Keep Going",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-         artist: "Swørn",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-         color: ["#CD607D", "#c94043"],
+         name:'Biyahe',
+         cover:'https://i.scdn.co/image/ab67616d0000b273a9b9ae859baf1ac430d8935c',
+         artist:'Jaysen x Chief',
+         color:['#E99B44','#CF1D25' ],
          id: uuidv4(),
          active: false,
+         audio:'https://audio.jukehost.co.uk/ny3lIvqIdb5w8QzVkagF4gElLPuYGZcE'
       },
       {
-         name: "Nightfall",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-         artist: "Aiguille",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-         color: ["#EF8EA9", "#ab417f"],
+         name: 'SAKIN KA NALANG',
+         cover: barcode_2,
+         artist: 'Pricetagg',
+         color: ['#FCFCFC', '#111111'],
          id: uuidv4(),
          active: false,
+         audio: 'https://audio.jukehost.co.uk/4TLhicNu1QhMctDJ2f0kuQTfHAKsfnRT'
       },
       {
-         name: "Reflection",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-         artist: "Swørn",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-         color: ["#CD607D", "#c94043"],
+         name: 'MUSIKA AT PERA',
+         cover: barcode_2,
+         artist: 'Pricetagg',
+         color: ['#FCFCFC', '#111111'],
          id: uuidv4(),
          active: false,
-      },
-      {
-         name: "Under the City Stars",
-         cover:
-            "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-         artist: "Aso, Middle School, Aviino",
-         audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-         color: ["#205950", "#2ab3bf"],
-         id: uuidv4(),
-         active: false,
-      },
-      //ADD MORE HERE
-      {
-         name: 'Apple',
-         cover: 'https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg',
-         artist: 'J. Folk',
-         color: ['#C2AA88', '#665E4D'],
-         id: uuidv4(),
-         active: false,
-         audio: 'https://mp3.chillhop.com/serve.php/?mp3=58833',
-      },
-      {
-         name: 'Steady',
-         cover: 'https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg',
-         artist: 'J. Folk',
-         color: ['#C2AA88', '#665E4D'],
-         id: uuidv4(),
-         active: false,
-         audio: 'https://mp3.chillhop.com/serve.php/?mp3=58834'
-
-      },
-      {
-         name: 'Trainride',
-         cover: 'https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg',
-         artist: 'J. Folk',
-         color: ['#C2AA88', '#665E4D'],
-         id: uuidv4(),
-         active: false,
-         audio: 'https://mp3.chillhop.com/serve.php/?mp3=58846'
-      },
-      {
-         name: 'In Circles',
-         cover: 'https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg',
-         artist: 'J. Folk',
-         color: ['#C2AA88', '#665E4D'],
-         id: uuidv4(),
-         active: false,
-         audio: 'https://mp3.chillhop.com/serve.php/?mp3=58836'
+         audio: 'https://audio.jukehost.co.uk/xOGf536uN2vZ0XmplhpRlMq9hNO3yW3A6'
       }, {
-         name: 'Offroad',
-         cover: 'https://chillhop.com/wp-content/uploads/2023/05/9e6aaa4e04b9e4054f8ed2586d331113e82247da-1024x1024.jpg',
-         artist: 'J. Folk',
-         color: ['#C2AA88', '#665E4D'],
+         name: 'EYES ON THE PRICE',
+         cover: barcode_2,
+         artist: 'Pricetagg',
+         color: ['#FCFCFC', '#111111'],
          id: uuidv4(),
          active: false,
-         audio: 'https://mp3.chillhop.com/serve.php/?mp3=58837'
+         audio: 'https://audio.jukehost.co.uk/T6qA5deigr7q6AFaS3IghviMAQ9HzYii'
+      },
+      {
+         name: 'GINTO',
+         cover: barcode_2,
+         artist: 'Pricetagg',
+         color: ['#FCFCFC', '#111111'],
+         id: uuidv4(),
+         active: false,
+         audio: 'https://audio.jukehost.co.uk/E5QZf5lU9s5f6tEIlpzZqCIOGbv0Tmj7'
+      },
+      {
+         name: 'HU U?',
+         cover: barcode_2,
+         artist: 'Pricetagg',
+         color: ['#FCFCFC', '#111111'],
+         id: uuidv4(),
+         active: false,
+         audio: 'https://audio.jukehost.co.uk/4HE6bgN3mre8DweKu9H3NkeXo6ILDh0F'
       }
 
 
